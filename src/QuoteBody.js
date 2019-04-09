4b8e98829e98afb5314b2f0594ce5c711a4468d0
Dominic Tremblay <dominictremblay24@gmail.com>
2019-04-09 01:22:26 -0400

    HTML Template divided into individual components

---
+++ src/QuoteBody.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import CommentList from './CommentList';
+
+const QuoteBody = props => {
+  return (
+    <div
+      id="d9424e04-9df6-4b76-86cc-9069ca8ee4bb"
+      className="collapse"
+      aria-labelledby="headingTwo"
+      data-parent="#quote-list"
+      aria-expanded="false"
+      style={{ height: '0px' }}>
+      <div className="card-body">
+        <h4>Comments</h4>
+
+        <CommentList />
+
+        <a
+          href="/quotes/d9424e04-9df6-4b76-86cc-9069ca8ee4bb/comments/new"
+          className="btn btn-info">
+          Add Comment
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default QuoteBody;
